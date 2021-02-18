61b66ea1d4431248c30727e30c8ab91568a529fe
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-02-18 20:19:13 -0300

    feat(app.js): create server

--- modules/app.js
+++ modules/app.js
@@ -49,3 +49,14 @@ logger.on("messageLogged", (eventDetails) => {
 
 logger.logMessage("message from app.js");
 
+const http = require('http');
+
+const server = http.createServer();
+server.listen(3000);
+
+server.on('connection', (soscket) => {
+    console.log('New Connection');
+})
+
+console.log('Listening on port 3000')
+
