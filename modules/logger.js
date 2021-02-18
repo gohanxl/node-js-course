c964a2b3f46b6961efda19a942735f39edf21739
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-02-18 20:11:20 -0300

    feat(logger.js): create Logger class with EventEmitter extension

--- modules/logger.js
+++ modules/logger.js
@@ -1,8 +1,24 @@
+const EventEmitter = require("events");
+
 const url = "http://mylogger.io/log";
 
-const logMessage = message => {
+class Logger extends EventEmitter {
+  logMessage = (message) => {
     console.log(message);
+    
+    this.emit("messageLogged", { id: 1, url: "http://" });
+  };
 }
 
-module.exports.logMessage = logMessage;
+// emitter.on("messageLogged", (eventDetails) => {
+//   console.log("Listener Called", eventDetails);
+// });
+
+// emitter.on("logging", ({ message }) => {
+//   console.log(message);
+// });
+
+// emitter.emit("logging", { message: "Message logged" });
+
+module.exports.Logger = Logger;
 // module.exports.endpoint = url;
